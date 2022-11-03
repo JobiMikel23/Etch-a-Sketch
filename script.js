e8e50135fd494291e7024b051bf762f66f47aab6
Mikel <115723692+JobiMikel23@users.noreply.github.com>
2022-11-03 13:15:32 +0000

    Create 16 divs with for loop

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const container = document.querySelector('#container');
-const testContent = document.createElement('p');
-testContent.textContent = "test";
-container.appendChild(testContent);
+// const testContent = document.createElement('p');
+// testContent.textContent = "test";
+// container.appendChild(testContent);
+
+//function to create intial 16 boxes
+function divGrid(){
+    for (let i = 0; i < 16; i++){
+        const square = document.createElement('div');
+        square.style.backgroundColor= "red";
+        square.style.borderColor = "black";
+        square.style.borderStyle = "solid";
+        square.className = "grid";
+        container.appendChild(square);
+    }
+}
+
+divGrid();
